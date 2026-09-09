5e522a949575e9ce4d22de3372668780bd1e4e16
Mostafa Ahmed <mostafafikry97@gmail.com>
2026-09-09 22:44:59 +0300

    update(checkRole) : updated logic of checkRole to handle change of role field in sessionClaims to roles which is of type array. also added a check to make sure that session claims has roles field

--- src/middlewares/checkRole.ts
+++ src/middlewares/checkRole.ts
@@ -2,17 +2,34 @@ import { getAuth } from "@clerk/express";
 import { NextFunction, Request, Response } from "express";
 import { validRoles } from "../constants/validRoles.js";
 
-export function checkRole(roles: (typeof validRoles)[number][]) {
-  return async (req: Request, res: Response, next: NextFunction) => {
-    const { sessionClaims } = getAuth(req);
-    const userMetaData = sessionClaims?.userMetadata as {
-      role: string;
-      dbUserId: string;
-    };
-    const role = userMetaData?.role as (typeof validRoles)[number];
+type Role = (typeof validRoles)[number];
 
-    if (!roles.includes(role))
-      return res.status(403).json({ message: "Unauthorized !!" });
+export function checkRole(allowedRoles: Role[]) {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const { sessionClaims } = getAuth(req);
+
+    const userMetadata = sessionClaims?.userMetadata as
+      | {
+          roles?: Role[];
+          dbUserId?: string;
+        }
+      | undefined;
+
+    const userRoles = userMetadata?.roles;
+
+    if (!userRoles || userRoles.length === 0) {
+      return res.status(403).json({
+        message: "User roles not found",
+      });
+    }
+
+    const isAuthorized = allowedRoles.some((role) => userRoles.includes(role));
+
+    if (!isAuthorized) {
+      return res.status(403).json({
+        message: "Forbidden",
+      });
+    }
 
     next();
   };
